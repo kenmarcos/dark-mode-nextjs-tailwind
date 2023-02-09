71b846eb8ac39beee7de1bad5c399e3ec5b371bb
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-02-09 14:09:14 -0300

    chore: add next-themes settings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import type { AppProps } from "next/app";
+import { Layout } from "@/components/Layout";
+import { ThemeProvider } from "next-themes";
 
 import "@/styles/globals.css";
-import { Layout } from "@/components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider attribute="class">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   );
 }
