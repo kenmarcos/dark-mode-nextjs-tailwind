c3e01f52b5cde05f60ec49d584ba905129d95f72
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-02-09 13:35:39 -0300

    feat: initialize Footer component layout

--- src/components/Layout/components/Footer/index.tsx
+++ src/components/Layout/components/Footer/index.tsx
@@ -1,3 +1,10 @@
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="py-6">
+      <div className="max-w-6xl mx-auto flex justify-center">
+        <span className="mr-2">Marcos Kenji Kuribayashi</span> &copy;{" "}
+        {new Date().getFullYear()} - Todos os direitos reservados
+      </div>
+    </footer>
+  );
 };
